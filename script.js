e6cd79b2a4a5c34a05f5fe7be724b3d6a2f48cd0
Muhammad Raffi Zeinuri Misbah <raffithea123@gmail.com>
2024-04-05 01:51:48 +0700

    Add About Page, Update JS, Add Description

--- script.js
+++ script.js
@@ -11,4 +11,16 @@ $(document).ready(function() {
             $(this).css('cursor', '');
         }
     );
+    $('button.nav-item.pesan').click(function() {
+        const nomorWhatsApp = '+6281312510136';
+        const pesanDefault = 'Halo%2C%20Saya%20ingin%20pesan%20Mie%20Jago!';
+        const url = `https://wa.me/${nomorWhatsApp}?text=${pesanDefault}`;
+        window.open(url, '_blank');
+    });
+    $('a.nav-link2.contact').click(function() {
+        const nomorWhatsApp = '+6281312510136';
+        const pesanDefault = 'Halo%2C%20Saya%20ingin%20Bertanya%20Perihal%20Mie%20Jago!';
+        const url = `https://wa.me/${nomorWhatsApp}?text=${pesanDefault}`;
+        window.open(url, '_blank');
+    });
 });
